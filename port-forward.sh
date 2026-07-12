1bd4d762a175418130d92f131a915efa77a9efcf
DreXtrime <tanel.neitov@gmail.com>
2026-07-12 10:39:16 +0300

    chore: update alertmanager config

--- port-forward.sh
+++ port-forward.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 kubectl port-forward -n monitoring service/prometheus-grafana 3000:80 &
 kubectl port-forward -n monitoring service/prometheus-kube-prometheus-prometheus 9091:9090 &
+kubectl port-forward -n monitoring service/prometheus-kube-prometheus-alertmanager 9093:9093 &
 kubectl port-forward -n cicd service/argocd-server 9090:443 &
-kubectl port-forward -n cicd service/argocd-server 5000:5000 &
 
 GRAFANA_PASSWORD=$(kubectl get secret -n monitoring prometheus-grafana -o jsonpath="{.data.admin-password}" | base64 -d)
 ARGO_PASSWORD=$(kubectl -n cicd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 echo "============================================="
-echo "Grafana: http://localhost:3000"
-echo "Password $GRAFANA_PASSWORD"
+echo "Grafana:      http://127.0.0.1:3000"
+echo "Password:     $GRAFANA_PASSWORD"
 echo ""
-echo "ArgoCD: https://localhost:9090"
-echo "Password $ARGO_PASSWORD"
+echo "ArgoCD:       https://127.0.0.1:9090"
+echo "Password:     $ARGO_PASSWORD"
 echo ""
-echo "Prometheus: https://localhost:9091"
-echo ""
+echo "Prometheus:   http://127.0.0.1:9091"
+echo "Alertmanager: http://127.0.0.1:9093"
 echo "============================================="
 echo "Press Ctrl+C to stop all port-forwards"
 wait
