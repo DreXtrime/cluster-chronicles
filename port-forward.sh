9eaa9dba9776636c98a17c6a5cbffca43e8919f0
DreXtrime <tanel.neitov@gmail.com>
2026-07-12 09:41:39 +0300

    chore: add labels to backend service

---
+++ port-forward.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+kubectl port-forward -n monitoring service/prometheus-grafana 3000:80 &
+kubectl port-forward -n monitoring service/prometheus-kube-prometheus-prometheus 9091:9090 &
+kubectl port-forward -n cicd service/argocd-server 9090:443 &
+
+GRAFANA_PASSWORD=$(kubectl get secret -n monitoring prometheus-grafana -o jsonpath="{.data.admin-password}" | base64 -d)
+ARGO_PASSWORD=$(kubectl -n cicd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
+echo "Grafana: http://localhost:3000"
+echo "Password $GRAFANA_PASSWORD"
+echo ""
+echo "ArgoCD: https://localhost:9090"
+echo "Password $ARGO_PASSWORD"
+echo ""
+echo "Prometheus: https://localhost:9091"
+echo ""
+echo "Press Ctrl+C to stop all port-forwards"
+wait
